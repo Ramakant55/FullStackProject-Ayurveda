eebafff01825cb034c457f84da2bde383e941a60
Ramakant55 <jangirramakant786@gmail.com>
2025-03-04 16:46:37 +0530

    loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,20 +3,13 @@ import './Loader.css';
 
 export const Loader = () => {
   return (
-    <div className="main">
-      <div className="up">
-        <div className="loaders">
-          {[...Array(10)].map((_, i) => (
-            <div key={i} className="loader"></div>
-          ))}
-        </div>
-        <div className="loadersB">
-          {[...Array(9)].map((_, i) => (
-            <div key={i} className="loaderA">
-              <div className={`ball${i}`}></div>
-            </div>
-          ))}
-        </div>
+    <div id="page">
+      <div id="container">
+        <div id="ring"></div>
+        <div id="ring"></div>
+        <div id="ring"></div>
+        <div id="ring"></div>
+        <div id="h3">loading</div>
       </div>
     </div>
   );
